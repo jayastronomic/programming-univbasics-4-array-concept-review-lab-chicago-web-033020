843ca621d529792b46bf82c05fb3d7b48d1038c2
Julian Antonio Smith <juliansmith94@hotmail.com>
2020-03-13 04:23:26 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,8 +3,12 @@ def find_element_index(array, value_to_find)
 end
 
 
-
-
+def find_max_value(array)
+  x = array[0]
+  array.length.times { |index|
+  if array[index] > x 
+    x = array[index]
+end
 
 
 
